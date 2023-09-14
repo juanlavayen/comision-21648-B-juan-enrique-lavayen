62727168b9c153e7c68ebd706285452b5e9c62c4
Juan Lavayen <juanlavayen@hotmail.com>
2023-09-14 19:50:36 -0300

    ActualizacionListado
    Se agrego campo de fecha de creacion al listado de posts

--- src/models/Post.js
+++ src/models/Post.js
@@ -4,7 +4,7 @@ const { DataTypes } = require("sequelize");
 const PostModel = sequelize.define("foro", {
   titulo_post: DataTypes.STRING,
   contenido_post: DataTypes.TEXT,
-  link_post: DataTypes.STRING 
+  link_post: DataTypes.STRING,
 });
 
 module.exports = { PostModel };
