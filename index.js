574fd025b3605e37307293f38bcc726f4b5edc29
Juan Lavayen <57622839+juanlavayen@users.noreply.github.com>
2023-09-13 23:59:10 -0300

    Actualizacion
    Comentarios-Identado-index.js-css

--- index.js
+++ index.js
@@ -6,25 +6,27 @@ require("./src/models/Post");
 
 const app = express();
 
-// middlewares
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cors());
-app.use(morgan("dev"));
+// Middlewares
+app.use(express.json()); // Middleware para procesar JSON
+app.use(express.urlencoded({ extended: false })); // Middleware para procesar datos de formularios
+app.use(cors()); // Middleware para habilitar CORS
+app.use(morgan("dev")); // Middleware para registrar solicitudes en la consola
 
-// para configurar en donde va a estar la carpeta de views
+// Configuración de la carpeta de vistas
 app.set("views", __dirname + "/src/views");
 
-// para configurar en donde va a estar la carpeta public
+// Configuración de la carpeta pública (archivos estáticos)
 app.use(express.static("public"));
 
-// // para configurar el motor de plantillas
+// Configuración del motor de plantillas EJS
 app.set("view engine", "ejs");
 
+// Rutas relacionadas con "/posts"
 app.use("/posts", require("./routes/posts.routes"));
 
+// Iniciar el servidor en el puerto 4000
 app.listen(4000, () => {
-  sequelize.sync({ force: false });
+  sequelize.sync({ force: false }); // Sincronizar la base de datos (no forzar la eliminación de datos existentes)
 
-  console.log("server on port 4000");
+  console.log("Servidor en el puerto 4000");
 });
